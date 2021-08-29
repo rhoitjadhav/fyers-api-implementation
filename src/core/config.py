7910f74aa9d2e4fabcd486b2b4102c50f92556f0
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-29 17:39:13 +0530

    configuration file

---
+++ src/core/config.py
@@ -0,0 +1,13 @@
+# Packages
+import os
+
+# Server Config
+HOST = "localhost"
+PORT = 8000
+SERVER_URL = "http://localhost:8000"
+
+# Fyers Credentials
+APP_ID = os.getenv("APP_ID")
+REDIRECT_URL = os.getenv("REDIRECT_URL")
+APP_SECRET = os.getenv("APP_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
