e8b0edddc67385d2a2d9902d08e470420a898c27
rhoitjadhav <jadhavrohit497@gmail.com>
2021-08-29 17:41:05 +0530

    entry point for running api

---
+++ src/main.py
@@ -0,0 +1,22 @@
+# Packages
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# Modules
+from api.api import router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", port=8000)
