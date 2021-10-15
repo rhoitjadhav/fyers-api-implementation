b2a78c30e0907e3594a9671b6a6fdc9f991cd097
rhoitjadhav <jadhavrohit497@gmail.com>
2021-10-15 17:23:36 +0530

    create tokens api

--- src/api/endpoints/access_token.py
+++ src/api/endpoints/access_token.py
@@ -7,11 +7,13 @@ from fastapi.responses import Response
 # Modules
 from core import config
 from utils.data_classes import ReturnValue
+from db.redis_database import RedisDatabase
 from api.usecases.access_token_usecase import AccessTokenUseCase
 
 router = APIRouter()
 
-access_token_usecase = AccessTokenUseCase(config.APP_ID, config.APP_SECRET, config.REDIRECT_URL)
+redis_database = RedisDatabase(config.REDIS_HOST, config.REDIS_PORT, config.REDIS_DB)
+access_token_usecase = AccessTokenUseCase(config.APP_ID, config.APP_SECRET, config.REDIRECT_URL, redis_database)
 
 
 @router.get("/generate-access-token")
@@ -38,14 +40,14 @@ async def generate_access_token(
                            error=traceback.format_exc())
 
 
-@router.get("/access-token")
-def access_token(response: Response):
+@router.get("/tokens")
+def tokens(response: Response):
     try:
-        _access_token = access_token_usecase.access_token
-        if _access_token:
-            return ReturnValue(True, "Access Token found", data=_access_token)
+        _tokens = access_token_usecase.get_tokens()
+        if _tokens.success:
+            return _tokens
 
-        return ReturnValue(False, "Access Token not found", data="")
+        return access_token_usecase.get_tokens_from_redis()
 
     except Exception as err:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
--- src/api/usecases/access_token_usecase.py
+++ src/api/usecases/access_token_usecase.py
@@ -1,19 +1,25 @@
 # Modules
 from utils.data_classes import ReturnValue
+from constants.redis_keys import RedisKeys
+from db.redis_database import RedisDatabase
 from fyers_api.accessToken import SessionModel
 
 
 class AccessTokenUseCase:
-    def __init__(self, app_id, app_secret, redirect_url):
+    def __init__(self, app_id: str, app_secret: str, redirect_url: str, redis_database: RedisDatabase):
         self._app_id = app_id
         self._app_secret = app_secret
         self._redirect_url = redirect_url
-
+        self._redis_database = redis_database
         self._access_token = None
 
     @property
     def access_token(self):
         return self._access_token
+
+    @property
+    def app_id(self):
+        return self._app_id
 
     def generate_access_token(self, auth_code: str) -> ReturnValue:
         grant_type = "authorization_code"
@@ -22,4 +28,37 @@ class AccessTokenUseCase:
         app_session.set_token(auth_code)
         response = app_session.generate_token()
         self._access_token = response["access_token"]
+
+        # Save tokens to Redis
+        mapping = {
+            RedisKeys.ACCESS_TOKEN: self._access_token,
+            RedisKeys.AUTHORIZATION: f"{self._app_id}:{self._access_token}"
+        }
+        self._redis_database.hset(RedisKeys.TOKENS, mapping=mapping)
+
         return ReturnValue(True, "Access Token Generated", data=self._access_token)
+
+    def get_tokens(self):
+        if not self._access_token:
+            return ReturnValue(False, "Tokens not found")
+
+        data = {
+            "app_id": self._app_id,
+            "access_token": self._access_token,
+            "authorization": f"{self._app_id}:{self._access_token}"
+        }
+        return ReturnValue(True, "Tokens found", data=data)
+
+    def get_tokens_from_redis(self):
+        tokens = self._redis_database.hgetall(RedisKeys.TOKENS)
+
+        if not tokens:
+            return ReturnValue(False, "Need to generate tokens first")
+
+        access_token = tokens[RedisKeys.ACCESS_TOKEN]
+        data = {
+            "app_id": self._app_id,
+            "access_token": access_token,
+            "authorization": f"{self._app_id}:{access_token}"
+        }
+        return ReturnValue(True, "Tokens found from redis", data=data)
