c293de05d44fee1ae76124300ba30af44bdf6670
rhoitjadhav <jadhavrohit497@gmail.com>
2021-10-15 17:26:04 +0530

    Fyers Credentials, redis configuration

--- src/core/config.py
+++ src/core/config.py
@@ -1,13 +1,18 @@
 # Packages
 import os
 
-# Server Config
+# API Server Config
 HOST = "localhost"
-PORT = 8000
+PORT = 8545
 SERVER_URL = f"http://{HOST}:{PORT}"
 
 # Fyers Credentials
-APP_ID = os.getenv("APP_ID")
-REDIRECT_URL = os.getenv("REDIRECT_URL")
-APP_SECRET = os.getenv("APP_SECRET")
-CLIENT_ID = os.getenv("CLIENT_ID")
+APP_ID = os.getenv("APP_ID", "BEPTBTGALL-100")
+REDIRECT_URL = os.getenv("REDIRECT_URL", f"http://{HOST}:{PORT}/generate-access-token")
+APP_SECRET = os.getenv("APP_SECRET", "DOXRLGHAG2")
+CLIENT_ID = os.getenv("CLIENT_ID", "XR09997")
+
+# Redis Config
+REDIS_HOST = "localhost"
+REDIS_PORT = 6390
+REDIS_DB = 0
