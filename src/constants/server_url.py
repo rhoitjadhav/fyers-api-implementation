4d65fbbe398b23573bd92dfe0f8840009ee80aa1
rhoitjadhav <jadhavrohit497@gmail.com>
2021-10-15 17:25:28 +0530

    server urls

---
+++ src/constants/server_url.py
@@ -0,0 +1,8 @@
+class ServerUrl:
+    # Fyers API Endpoints
+    FYERS_HOST = "https://api.fyers.in"
+    ENDPOINT_HISTORICAL_DATA = "/data-rest/v2/history"
+
+    # API Server Endpoints
+    ENDPOINT_HISTORICAL_DATA_API_SERVER = "/historical-data"
+    ENDPOINT_TOKENS_API_SERVER = "/tokens"
