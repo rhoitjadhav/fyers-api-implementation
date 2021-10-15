58cd0c406e988ed662a5713d8381273d526e6ce6
rhoitjadhav <jadhavrohit497@gmail.com>
2021-10-15 17:24:28 +0530

    get_access_authorization() function

--- src/apps/market_feed/main.py
+++ src/apps/market_feed/main.py
@@ -4,11 +4,8 @@ from datetime import datetime
 
 # Modules
 from utils.data_classes import ReturnValue
+from constants.server_url import ServerUrl
 from fyers_api.websocket.ws import FyersSocket
-
-
-def _print_symbol_feed(self):
-    print(self.response)
 
 
 class MarketFeed:
@@ -18,28 +15,28 @@ class MarketFeed:
         self._server_url = server_url
 
         self._access_token = None
+        self._access_authorization = None
         self._fyers_socket = None
 
-    def _get_access_token(self):
-        if self._access_token:
-            return self._access_token
+    def _get_access_authorization(self):
+        if self._access_authorization:
+            return self._access_authorization
 
         # Request API server for access token
-        url = self._server_url + "/access-token"
+        url = self._server_url + ServerUrl.ENDPOINT_TOKENS_API_SERVER
 
         response = requests.get(url)
         if response.status_code != 200:
-            return ReturnValue(False, "Error while getting access token", error=response.reason)
+            return ReturnValue(False, "Error while getting access authorization", error=response.reason)
 
         response = response.json()
         if response["success"]:
-            return ReturnValue(True, data=response["data"])
+            return ReturnValue(True, data=response["data"]["authorization"])
 
-        return ReturnValue(False, "Error while getting access token", error=response["error"])
+        return ReturnValue(False, "Error while getting access authorization", error=response["error"])
 
     @staticmethod
-    def _print_symbol_feed(feed):
-        # print(feed.response)
+    def print_symbol_feed(feed):
         for r in feed.response:
             t = datetime.fromtimestamp(r['last_traded_time']).strftime("%Y-%m-%d %H:%M:%S")
             ltt = f"LTT: {t}"
@@ -50,18 +47,17 @@ class MarketFeed:
             msg = f"  {sym}-> {ltp}, {vol}, {ltt}, {bqt}"
             print(msg, end="\r")
 
-    def start_feed(self):
-        access_token = self._get_access_token().data
-        if not access_token:
-            return_value = ReturnValue(False, "Token not found")
+    def start_feed(self, feed_caputring_object):
+        access_authorization = self._get_access_authorization().data
+        if not access_authorization:
+            return_value = ReturnValue(False, "Access Authorization not found")
             print(return_value)
             return
 
-        _access_token = f"{self._app_id}:{access_token}"
         data_type = "symbolData"
 
-        FyersSocket.websocket_data = self._print_symbol_feed
+        FyersSocket.websocket_data = feed_caputring_object
 
         print(f"Feed Started for symbols: {self._symbols}")
-        self._fyers_socket = FyersSocket(access_token=_access_token, data_type=data_type, symbol=self._symbols)
+        self._fyers_socket = FyersSocket(access_token=access_authorization, data_type=data_type, symbol=self._symbols)
         self._fyers_socket.subscribe()
