41b3e791493705d013a8dacdd24bc795a36199c2
rhoitjadhav <jadhavrohit497@gmail.com>
2021-10-15 17:23:57 +0530

    historical data api

--- src/api/api.py
+++ src/api/api.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 
 # Modules
 from .endpoints.auth_code import router as auth_code_router
+from .endpoints.historical_data import router as historical_data
 from .endpoints.access_token import router as generate_access_token_router
 
 router = APIRouter()
 
 router.include_router(auth_code_router)
+router.include_router(historical_data)
 router.include_router(generate_access_token_router)
