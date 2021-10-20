f4d729180d175b158681864e5df3952ce6d8c886
rhoitjadhav <jadhavrohit497@gmail.com>
2021-10-20 19:43:36 +0530

    ohlcv notebook

--- src/constants/server_url.py
+++ src/constants/server_url.py
@@ -4,5 +4,6 @@ class ServerUrl:
     ENDPOINT_HISTORICAL_DATA = "/data-rest/v2/history"
 
     # API Server Endpoints
+    ENDPOINT_GENERATE_AUTH_CODE = "/generate-auth-code"
     ENDPOINT_HISTORICAL_DATA_API_SERVER = "/historical-data"
     ENDPOINT_TOKENS_API_SERVER = "/tokens"
