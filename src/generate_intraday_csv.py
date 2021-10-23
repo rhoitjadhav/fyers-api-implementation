4e4904d5c1ee761e9498ff136d70f8c582173f8f
rhoitjadhav <jadhavrohit497@gmail.com>
2021-10-23 10:51:58 +0530

    reading inputs from commandline

--- src/generate_intraday_csv.py
+++ src/generate_intraday_csv.py
@@ -1,10 +1,14 @@
+import sys
 import json
 import pandas as pd
 from db.redis_database import RedisDatabase
 
 r = RedisDatabase()
 
-timeseries_data = r.hgetall("NSE:OFSS-EQ")
+symbol = sys.argv[1]
+filename = sys.argv[2]
+
+timeseries_data = r.hgetall(symbol)
 
 columns = ["timecode", "price", "volume"]
 rows = []
@@ -20,4 +24,4 @@ df = pd.DataFrame(rows, columns=columns)
 df.set_index("timecode", inplace=True)
 df.index = pd.to_datetime(df.index)
 
-df.to_csv("ofss.csv", header=True)
+df.to_csv(filename, header=True)
