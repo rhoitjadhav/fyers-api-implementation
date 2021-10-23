16ffc8ec0c848a039ef6e83689a7dd4a72b2f3ca
rhoitjadhav <jadhavrohit497@gmail.com>
2021-10-23 19:01:42 +0530

    added zmq configuration

--- src/core/config.py
+++ src/core/config.py
@@ -5,19 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # API Server Config
-HOST = "localhost"
-PORT = 8545
-SERVER_URL = f"http://{HOST}:{PORT}"
+API_SERVER_HOST = os.getenv("API_SERVER_HOST")
+API_SERVER_PORT = int(os.getenv("API_SERVER_PORT"))
+API_SERVER_URL = f"http://{API_SERVER_HOST}:{API_SERVER_PORT}"
 
 # Fyers Credentials
 FYERS_USERNAME = os.getenv("FYERS_USERNAME")
 FYERS_PASSWORD = os.getenv("FYERS_PASSWORD")
 PAN_ID = os.getenv("PAN_ID")
 APP_ID = os.getenv("APP_ID")
-REDIRECT_URL = os.getenv("REDIRECT_URL", f"http://{HOST}:{PORT}/generate-access-token")
+REDIRECT_URL = os.getenv("REDIRECT_URL", f"http://{API_SERVER_HOST}:{API_SERVER_PORT}/generate-access-token")
 APP_SECRET = os.getenv("APP_SECRET")
 
 # Redis Config
-REDIS_HOST = "localhost"
-REDIS_PORT = 6390
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = int(os.getenv("REDIS_PORT"))
 REDIS_DB = 0
+
+# ZMQ Configuration
+ZMQ_HOST = os.getenv("ZMQ_HOST")
+ZMQ_PORT = int(os.getenv("ZMQ_PORT"))
